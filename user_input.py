4cf18d07fe1974165f71d34121c211df4568c55d
NattieKavita <nataliakavita@gmail.com>
2024-10-31 22:59:00 +0300

    WEEK 1 DONE

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name")
+age = int(input("What is your age"))
+location = input("Enter your location")
+
+print(f"Hi{name}, you are{age} years old and you live on {location}")
+
