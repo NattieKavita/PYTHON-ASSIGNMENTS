f2ec4214709b1a50ff94388a82fe5a4d46f55f47
NattieKavita <nataliakavita@gmail.com>
2024-11-10 21:24:08 +0300

    WEEK 2 DONE

---
+++ lists.py
@@ -0,0 +1,40 @@
+# 1. Empty list
+
+my_list = [] 
+
+# 2 Appending items 
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+
+
+# 3 insert 15 at the second position
+my_list.insert(1,15)
+
+
+
+# 4 extending list
+list_2 = [50, 60, 70]
+
+my_list.extend(list_2)
+
+
+
+# 5 remove last element from list 
+del my_list[-1]
+
+
+
+# 6 sort list in ascending order
+
+my_list.sort()
+
+print(my_list)
+
+# 7 print the index of the value 30 
+
+print(my_list[3])
+
+
